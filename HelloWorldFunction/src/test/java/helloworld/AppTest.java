194084587094ac4166b6b73e54a375cd3fcf24d4
Kevin Richardson <krichardson@turnitin.com>
2025-01-15 12:49:09 -0500

    Test new cache mechanism
    * Test https://github.com/turnitin/github-actions/pull/365

--- HelloWorldFunction/src/test/java/helloworld/AppTest.java
+++ HelloWorldFunction/src/test/java/helloworld/AppTest.java
@@ -16,7 +16,7 @@ public class AppTest {
     String content = result.getBody();
     assertNotNull(content);
     assertTrue(content.contains("\"message\""));
-    assertTrue(content.contains("\"hello world 2\""));
+    assertTrue(content.contains("\"hello world 3\""));
     assertTrue(content.contains("\"location\""));
   }
 }
